b7cbba89e9db03ba28b1058877699e249e7f28f3
Indexmod <23720911+indexmod@users.noreply.github.com>
2026-03-30 17:33:53 +0300

    Update topics.js

--- 01_TOPICS_BOARD/topics.js
+++ 01_TOPICS_BOARD/topics.js
@@ -1,14 +1,12 @@
 // 01_TOPICS_BOARD/topics.js
 
-async function loadTopics() {
-  const res = await fetch('./topics.txt');
+export async function loadTopics() {
+  const res = await fetch('/mode/01_TOPICS_BOARD/topics.txt');
   const text = await res.text();
 
-  const topics = text
+  return text
     .split('\n')
     .map(t => t.trim())
     .filter(Boolean)
-    .sort((a, b) => a.localeCompare(b));
-
-  return topics;
+    .sort();
 }
