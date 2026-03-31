2b9cf8dd7e5ef50ac07eb6ec08d19cd60e04d4d2
indexmod <23720911+indexmod@users.noreply.github.com>
2026-03-31 10:04:17 +0300

    123

--- 03_SECURE_BOX/providers/auth.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-function getKey(name) {
-  const key = process.env[name];
-  if (!key) throw new Error(`Missing env key: ${name}`);
-  return key;
-}
-
-module.exports = { getKey };
--- 03_SECURE_BOX/providers/test.js
+++
@@ -1,6 +0,0 @@
-const { generateText } = require("./client");
-
-(async () => {
-  const res = await generateText("Write a short poem about AI");
-  console.log(res);
-})();
